67eae82ab0eaab031364e07e4cccc07cb0685713
Md.Sohan Mia <s20111102@bsfmstu.ac.bd>
2025-01-26 16:57:32 +0600

    Task1 Done

--- Folder1/Task1.py
+++ Folder1/Task1.py
@@ -1,14 +1,20 @@
-def fibonacci_generator(n):
-    a,b=0,1
+"""                       TASK 1
+
+                  FIBONACCI GENERATOR
+
+The Fibonacci series is a sequence where each number is
+the sum of the two preceding numbers, defined by a
+mathematical recurrence relationship."""
+# 0 1 1 2 3 5 8 
+# if the previous number a and b the next number c= a+b
+def fibonacc_generator(n):
+    a,b = 0,1
     fib_sequence = ['0']
-    
-    for i in range(0,n):
+    for i in range(1,n):
         c=a+b
         a=b
         b=c
         fib_sequence.append(a)
-    print("["," ".join(map(str, fib_sequence)),"]")
-    
-fibonacci_generator(10)
-#0 1 1 2 3 5
-#0 1 2 3 4 5 
+    print("["," ".join(map(str,fib_sequence)),"]")
+
+fibonacc_generator(10)
